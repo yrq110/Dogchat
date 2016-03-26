f9dd1e311b6461d847838e276ee4aceee347831a
yrq110 <yrq1025@gmail.com>
2016-03-26 23:54:00 +0800

    Initial Commit

---
+++ WildDogExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WildDogExample
+//
+//  Created by yrq_mac on 16/3/26.
+//  Copyright © 2016年 yrq_mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
